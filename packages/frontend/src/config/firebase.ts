c5003d8e9e49ae08c9aaf66924313141cf881f17
Felix Sundqvist <48220704+FelixSundqvist@users.noreply.github.com>
2025-02-15 20:33:40 +0200

    chore(frontend): add appcheck

--- packages/frontend/src/config/firebase.ts
+++ packages/frontend/src/config/firebase.ts
@@ -2,6 +2,13 @@ import { initializeApp } from "firebase/app";
 import { connectAuthEmulator, getAuth } from "firebase/auth";
 import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+
+declare global {
+  interface Window {
+    FIREBASE_APPCHECK_DEBUG_TOKEN: boolean;
+  }
+}
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
@@ -12,14 +19,26 @@ const firebaseConfig = {
     .VITE_FIREBASE_MESSAGING_SENDER_ID as string,
   appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
 };
+
+if (import.meta.env.DEV) {
+  window.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+}
 const app = initializeApp(firebaseConfig);
+
+initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(
+    import.meta.env.VITE_FIREBASE_RECAPTCHA_SITE_KEY as string,
+  ),
+  isTokenAutoRefreshEnabled: true,
+});
+
 const auth = getAuth(app);
 
 const functions = getFunctions(app);
 
 const db = getFirestore(app);
 
-if (import.meta.env.MODE === "development") {
+if (import.meta.env.DEV) {
   connectAuthEmulator(auth, "http://localhost:9099", { disableWarnings: true });
   connectFunctionsEmulator(functions, "localhost", 5001);
   connectFirestoreEmulator(db, "localhost", 8080);
